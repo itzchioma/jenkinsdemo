6ccd6df930ca3f9e1089ab40bef5764a70216c82
tariq <tariq@tariqs-MacBook-Pro.local>
2023-09-23 20:30:29 +0530

    initial commit

---
+++ deployment/main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Fetching the VPC named Application-Vpc
+data "aws_vpc" "selected" {
+  filter {
+    name   = "tag:Name"
+    values = ["Application-Vpc"]
+  }
+}
+
+# Fetching subnets of the VPC
+data "aws_subnets" "selected" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.selected.id]
+  }
+}
+
+# Fetching the latest Amazon Linux 2 AMI
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"] # Amazon's owner ID
+}
+
+# Create EC2 instance
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = "t2.micro"
+
+  # Using the first subnet from the fetched list
+  subnet_id = tolist(data.aws_subnets.selected.ids)[0]
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
